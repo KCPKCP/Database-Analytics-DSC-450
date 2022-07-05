0e8b612d6a90faef52eb0a5f203d5852745a60cd
Keiland P <kcpullen@comcast.net>
2022-07-05 17:04:43 -0500

    Add files via upload

---
+++ HW4/KCPullen_HW4_Part1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  2 21:00:29 2022
+
+@author: Home
+"""
+
+"""
+for animal in f_animals:
+    if "tiger" in animal:
+        if "common" not in animal:
+            print(animal)
+"""
+
+"""
+f = open("animal.txt", "r")
+
+f_animals = f.readlines()
+
+for animal in f_animals:
+    if "tiger" not in animal:
+        print(animal)
+"""
